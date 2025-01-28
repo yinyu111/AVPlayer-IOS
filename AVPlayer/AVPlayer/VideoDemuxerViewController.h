7608807d6365068f66d9884ac1d9789074136570
kaku <yyharbin@163.com>
2025-01-28 19:36:01 +0800

    feat：视频解封装

---
+++ AVPlayer/AVPlayer/VideoDemuxerViewController.h
@@ -0,0 +1,21 @@
+//
+//  VideoDemuxerViewController.h
+//  AVPlayer
+//
+//  Created by 尹玉 on 2025/1/24.
+//
+
+#ifndef VideoDemuxerViewController_h
+#define VideoDemuxerViewController_h
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VideoDemuxerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif /* VideoDemuxerViewController_h */
--- AVPlayer/AVPlayer/VideoEncoderViewController.h
+++ AVPlayer/AVPlayer/VideoEncoderViewController.h
@@ -1,21 +1,21 @@
+////
+////  VideoEncoderViewController.h
+////  AVPlayer
+////
+////  Created by 尹玉 on 2025/1/22.
+////
 //
-//  VideoEncoderViewController.h
-//  AVPlayer
+//#ifndef VideoEncoderViewController_h
+//#define VideoEncoderViewController_h
 //
-//  Created by 尹玉 on 2025/1/22.
+//#import <UIKit/UIKit.h>
 //
-
-#ifndef VideoEncoderViewController_h
-#define VideoEncoderViewController_h
-
-#import <UIKit/UIKit.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface VideoEncoderViewController : UIViewController
-
-@end
-
-NS_ASSUME_NONNULL_END
-
-#endif /* VideoEncoderViewController_h */
+//NS_ASSUME_NONNULL_BEGIN
+//
+//@interface VideoEncoderViewController : UIViewController
+//
+//@end
+//
+//NS_ASSUME_NONNULL_END
+//
+//#endif /* VideoEncoderViewController_h */
